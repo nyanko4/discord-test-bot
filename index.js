2351d38ff58070d4ed56c588ad77633c2ab5737d
nyanko4 <nyankobetsuaka@outlook.com>
2026-04-08 22:40:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Bot is running');
+})
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
@@ -16,4 +22,16 @@ client.on('messageCreate', (message) => {
     }
 });
 
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName == 'hello') {
+        await interaction.reply('こんにちは！');
+    }
+})
+
 client.login(process.env.DISCORD_APITOKEN);
+
+app.listen(3000, () => {
+    console.log(`${process.pid} started`);
+})
