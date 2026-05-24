1449b01fcd06f9281000fafff50c743b6a399ce0
nyanko4 <nyankobetsuaka@outlook.com>
2026-05-24 22:00:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function judgeChinchiro(a, b, c) {
 }
 
 async function diceRoll(interaction) {
-    interaction.deferReplay();
+    interaction.deferReply();
     console.log(interaction);
     const rolls = Array.from({ length: 3 }, () =>
         Math.floor(Math.random() * 6) + 1
